e0461d96f13badd15bdc86a9d94f03a70a8f98d5
Paul Sterl <paul@sterl.org>
2022-08-28 22:35:56 +0200

    cleanup

--- core/src/main/java/org/sterl/pmw/model/WorkflowChooseFunction.java
+++ core/src/main/java/org/sterl/pmw/model/WorkflowChooseFunction.java
@@ -1,7 +1,5 @@
 package org.sterl.pmw.model;
 
-import java.util.function.Consumer;
-
 @FunctionalInterface
 public interface WorkflowChooseFunction<StateType extends WorkflowState> {
     String apply(StateType state, WorkflowContext context);
